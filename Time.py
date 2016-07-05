c0f3ae3f75c8491dffe89dad1535c52a4beb17a6
ganglin.song <ganglin.song@yourdream.cc>
2016-07-05 19:20:56 +0800

    add code

---
+++ Time.py
@@ -0,0 +1,17 @@
+#!/usr/bin/env python3
+#-*-encoding:utf8-*-
+
+
+from datetime import datetime;
+#当前时间
+print(datetime.now());
+
+print(datetime(2016,12,12,12,12,12));
+
+print(datetime.now().timestamp());
+print(datetime.fromtimestamp(datetime.now().timestamp()));
+#strtotime
+print(datetime.strptime('2016-12-02 19:23:28','%Y-%m-%d %H:%M:%S'));
+
+#time to string
+print(datetime.now().strftime('%Y-%m-%d %H:%M:%S'));
